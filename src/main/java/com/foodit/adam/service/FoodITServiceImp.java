cf17e7598ffcf802ddfebbc118e0dc05af25dc95
CaramelAdam <Adam@Adam>
2014-05-20 15:36:55 +0100

    proper services!

---
+++ src/main/java/com/foodit/adam/service/FoodITServiceImp.java
@@ -0,0 +1,61 @@
+package com.foodit.adam.service;
+
+import com.foodit.adam.model.Meal;
+import com.foodit.adam.model.Order;
+import com.foodit.test.sample.controller.RestaurantData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class FoodITServiceImp implements FoodITService {
+	
+	public Integer getMostOrderedMealID() {
+		Collection<Order> allOrders = getAllOrders();
+		
+		Iterator it = allOrders.iterator();
+		
+		Integer mostCommonOrderID = 0;
+		Integer counter= 0;
+		HashMap<Integer, Integer> mealOccurrence = new HashMap<Integer, Integer>();
+		
+		while (it.hasNext()) {//go through all orders counting occurrence of meals
+			Order order = (Order) it.next();
+			Collection<Meal> orderedMeals = order.getLineItems();
+			
+			for(Meal meal:orderedMeals) {//get the number of times a meal appears
+				Integer mealID = meal.getId();
+				if (mealOccurrence.containsKey(mealID)) {
+					//up the number of occurrence
+					Integer numberOfMealOccurances = mealOccurrence.get(mealID);
+					numberOfMealOccurances = numberOfMealOccurances + 1;
+					mealOccurrence.put(mealID, numberOfMealOccurances );
+					
+					//if it's highest number of occurrence then set it
+					//doing this here, so we can just do one pass.
+					if (numberOfMealOccurances > counter && mealID !=null) {
+						mostCommonOrderID = mealID;
+						counter = numberOfMealOccurances;
+					}
+					
+				}else{
+					mealOccurrence.put(mealID, 1);
+				}
+			}
+		}		
+		return mostCommonOrderID;
+	}
+	
+	public Collection<Order> getAllOrders(){
+		RestaurantService rs = new RestaurantServiceImp();
+		Collection<RestaurantData> restaurantData = rs.getRestaurants();
+		Collection<Order> allOrders = new ArrayList<Order>();
+		for(RestaurantData restaurant: restaurantData) {
+			Collection<Order> restaurantOrders = rs.getOrders(restaurant);
+			allOrders.addAll(restaurantOrders);
+		}
+		return allOrders;
+	}
+
+}
--- src/main/java/com/foodit/adam/service/RestaurantService.java
+++ src/main/java/com/foodit/adam/service/RestaurantService.java
@@ -4,137 +4,28 @@ import com.foodit.adam.model.Meal;
 import com.foodit.adam.model.Menu;
 import com.foodit.adam.model.Order;
 import com.foodit.test.sample.controller.RestaurantData;
-import com.google.appengine.labs.repackaged.org.json.JSONException;
-import com.google.appengine.labs.repackaged.org.json.JSONObject;
-import com.google.gson.Gson;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import com.google.gson.reflect.TypeToken;
+public abstract interface RestaurantService {
 
-import java.lang.reflect.Type;
+	public abstract Collection<RestaurantData> getRestaurants();
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
+	public abstract RestaurantData getRestaurant(String name);
 
-public class RestaurantService {
+	public abstract Collection<Order> getOrders(String restaurant);
 
-	public Collection<RestaurantData> getRestaurants() {
-		return ofy().load().type(RestaurantData.class).list();
-	}
-
-	public RestaurantData getRestaurant(String name) {
-		return ofy().load().type(RestaurantData.class).filter("restaurant ==", name).list().get(0);
-	}
-
-	public Collection<Order> getOrders(String restaurant) {
-		RestaurantData rd = getRestaurant(restaurant);
-		Collection<Order> orders = getOrders(rd);
-		return orders;
-	}
-
-	public Collection<Order> getOrders(RestaurantData rd) {
-		Gson gson = new Gson();
-		Type type = new TypeToken<Collection<Order>>(){}.getType();
-		String orderString = rd.getOrdersJson().getValue();
-		Collection<Order> orders = gson.fromJson(orderString, type );
-		return orders;
-	}
+	public abstract Collection<Order> getOrders(RestaurantData rd);
 	
-	public BigDecimal getTotalSales(String restaurant) {
-		Collection<Order> orders = getOrders(restaurant);
-		BigDecimal total = new BigDecimal(0.0);
-		for(Order order:orders) {
-			total = total.add(order.getTotalValue());
-		}
-		return total;
-	}
+	public abstract BigDecimal getTotalSales(String restaurant);
 	
-	public Menu getMenu(String restaurant) {
-		RestaurantData rd = getRestaurant(restaurant);
-		Menu menu = getMenu(rd);
-		return menu;
-	}
+	public abstract Menu getMenu(String restaurant);
 	
-	public Menu getMenu(RestaurantData rd)  {
-		String menuString = rd.getMenuJson().getValue();
-		String restaurantID = "";
-		Collection<Meal> meals = new ArrayList<Meal>();
-		try {
-		    JSONObject jObject  = new JSONObject(menuString);
-		    JSONObject menu;
-			menu = jObject.getJSONObject("menu");//get the menu
-			restaurantID = jObject.get("restaurantId").toString();////get the restaurantID
-			
-			//get the map of categories (e.g. kebab:type of kebabs available)
-			//This is because each restaurant can have different categories 
-			//so we have to take the long way round of dynamically dealing with the category json keys, rather than hardcoding (e.g. orders)
-		    Map<String,String> categoryMap = new HashMap<String,String>();
-		    Iterator menuIterator = menu.keys();
-		    while(menuIterator.hasNext()){
-		        String key = (String)menuIterator.next();
-		        String value = menu.getString(key);
-		        categoryMap.put(key,value);
-		        
-		    }
-	        //go through the type of kebabs available and convert them to meals, finally adding 
-		    Iterator categoryIterator = categoryMap.entrySet().iterator();
-		    while(categoryIterator.hasNext()){
-		    	
-		    	Entry  category = (Entry) categoryIterator.next();
-		    	String categoryKey = category.getKey().toString();
-		    	String categoryValues = category.getValue().toString();
-		    	
-				Gson gson = new Gson();
-				Type type = new TypeToken<Collection<Meal>>(){}.getType();
-				Collection<Meal> categoryMeals = gson.fromJson(categoryValues, type );
-				
-			    for(Meal meal : categoryMeals) {
-			    	meals.add(meal);
-			    }
-		    }
-			return new Menu(meals, restaurantID);
-		    
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}				
-	}	
+	public abstract Menu getMenu(RestaurantData rd);
 	
-	public Collection<Meal> getMeals(String restaurant) {
-		Menu menu = getMenu(restaurant);
-		Collection<Meal> meals = getMenu(restaurant).getMenu();
-		return meals;
-	}
+	public abstract Collection<Meal> getMeals(String restaurant);
 	
-	public String getMostOrderedCategory(String restaurant) {
-		
-		Collection<Meal> meals = getMeals(restaurant);
-		String mostCommonCategory= "";
-		Integer categoryValue= 0;
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for(Meal meal:meals) {//get the number of times a category appears
-			String category = meal.getCategory();
-			if (map.containsKey(category)) {
-				Integer val =  map.get(category)+1;
-				map.put(category, val);
-				
-				if (val > categoryValue) {
-			    	mostCommonCategory = category;
-			    	categoryValue = val;
-			    }
-				
-			}else{
-				map.put(category, 1);
-			}
-		}		
-		return mostCommonCategory;
-	}
+	public abstract String getMostOrderedCategory(String restaurant);
 	
 }
